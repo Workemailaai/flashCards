dadab5b3a7fe85745c1a18b20ef9041f789986fe
Pavel-Sk1 <pavel.sk1995@yandex.ru>
2025-08-29 01:24:55 +0400

    add second page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import Layout from './app/Layout'
 import HomePage from './pages/home/HomePage'
+import DeckPage from './pages/Deck/DeckPage'
 
 function App() {
   
   return (
     <>
       <Routes>
-      <Route path='/' element={<Layout />}>        
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
+        <Route path='/' element={<Layout />}>        
+          <Route index element={<HomePage />} />
+          <Route path='decks' element={<DeckPage />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/Deck/DeckPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import './DeckPage.css';
+
+export default function DeckPage() {
+//   const navigate = useNavigate();
+
+  return (
+    <h2>ЧЛЕН2</h2>
+  );
+}
